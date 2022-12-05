35b6e36d1ec0c726b7d21dd10640d01ed38f8a3d
skandog <muumrar@gmail.com>
2022-12-05 16:28:48 +0000

    getting there - converting ranging intto list of ints, then checking if one array contained in another

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var puzzleInput = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8";
 var puzzleInputArr = puzzleInput.split('\n');
 var parsedPuzzleInputArr = [];
+// turn hyphenated range into array of numbers
+function range(s, f) {
+    return __spreadArray([], Array(f - s + 1), true).map(function (e, i) { return i + s; });
+}
+// check if one array contained in another
+var checker = function (arr, target) { return target.every(function (v) { return arr.includes(v); }); };
 for (var i = 0; i < puzzleInputArr.length; i++) {
     parsedPuzzleInputArr[i] = puzzleInputArr[i].split(',');
+    var rangeOneLow = Number(parsedPuzzleInputArr[i][0].charAt(0));
+    var rangeOneHi = Number(parsedPuzzleInputArr[i][0].charAt(2));
+    var rangeTwoLow = Number(parsedPuzzleInputArr[i][1].charAt(0));
+    var rangeTwoHi = Number(parsedPuzzleInputArr[i][1].charAt(2));
+    var rangeOne = range(rangeOneLow, rangeOneHi);
+    var rangeTwo = range(rangeTwoLow, rangeTwoHi);
+    console.log(checker(rangeOne, rangeTwo));
+    console.log(checker(rangeTwo, rangeOne));
+    console.log("----");
 }
 console.log(parsedPuzzleInputArr);
--- index.ts
+++ index.ts
@@ -11,8 +11,28 @@ let puzzleInputArr = puzzleInput.split('\n');
 
 let parsedPuzzleInputArr = []
 
+// turn hyphenated range into array of numbers
+function range(s,f){
+    return [...Array(f-s+1)].map((e,i)=>i+s)
+}
+
+// check if one array contained in another
+let checker = (arr, target) => target.every(v => arr.includes(v));
+
+
 for (let i = 0; i < puzzleInputArr.length; i++) {
     parsedPuzzleInputArr[i] = puzzleInputArr[i].split(',')
+    let rangeOneLow =  Number(parsedPuzzleInputArr[i][0].charAt(0))
+    let rangeOneHi =  Number(parsedPuzzleInputArr[i][0].charAt(2))
+    let rangeTwoLow =  Number(parsedPuzzleInputArr[i][1].charAt(0))
+    let rangeTwoHi =  Number(parsedPuzzleInputArr[i][1].charAt(2))
+    let rangeOne =  range(rangeOneLow, rangeOneHi)
+    let rangeTwo =  range(rangeTwoLow, rangeTwoHi)
+    
+
+    console.log(checker(rangeOne, rangeTwo));
+    console.log(checker( rangeTwo, rangeOne));
+    console.log("----");
 }
 
 
